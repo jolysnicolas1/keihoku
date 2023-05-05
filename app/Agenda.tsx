606718d8146804837b520209048b90c1f98ae1c1
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-05 14:51:04 +0200

    Addeed vector.png Added Agenda coponent. Commit before installing flowbite

---
+++ app/Agenda.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import logo from 'public/vector.png'
+
+export default function Agenda() {
+  return (
+    <div className='bg-sky-300 z-20 flex flex flex-col w-1/2 start-0'>
+        agenda
+
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
+import logo from 'public/vector.png'
+import Agenda from './Agenda'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Keihoku Studio</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+      <Image
+      className='absolute z-0'
+      src={logo}
+      width={1100}
+      height={1100}
+      alt="logo"
+      />
+      <Agenda></Agenda>
     </main>
   )
 }
