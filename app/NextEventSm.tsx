6de05753aac0c2b767ea7de9003f3210c4a72489
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-09 20:43:14 +0200

    Add maru picture

--- app/NextEventSm.tsx
+++ app/NextEventSm.tsx
@@ -3,10 +3,24 @@ import maru from 'public/maru.webp'
 
 export default function NextEvent() {
   return (
-        <div className="w-screen border-y-4 border-l-4 border-blue-500">
-            <div className="text-blue-500">
-                <p>MOLECULAR.DANCE/BUTOH.</p>
-                <p>WORKSHOP/MARUSKA.RONCHI</p>
+        <div className="w-screen border-y-4 border-l-4 border-blue-500 flex flex-col align-center text-xl space-y-3 p-2">
+            <div className="font-medium">
+                <p className='text-cyan-200'>MOLECULAR.DANCE/BUTOH.WORKSHOP</p>
+                <p className='text-blue-500'>/MARUSKA.RONCHI</p>
+            </div>
+            <div className="flex flex-col items-center">
+                <div className="flex space-x-4 text-neutral-400 font-medium text-lg">
+                    <p>/02-06/2023</p>
+                    <p>/03-06/2023</p>
+                    <p>/04-06/2023</p>
+                </div>
+                <Image
+                    className=''
+                    src={maru}
+                    height={500}
+                    width={400}
+                    alt="logo"
+                    />
             </div>
         </div>
   )
