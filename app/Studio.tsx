d7d952ab2afb3c0ee8ee61dea7558d6e74971be6
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-10 11:32:02 +0200

    Main card done

--- app/Studio.tsx
+++ app/Studio.tsx
@@ -12,11 +12,11 @@ export default function Studio() {
     //     <p className='px-16 text-neutral-400 inline-block -mb-1 text-lg'>La Dérive</p>
     //   </div>
     // </div>
-    <div className={`flex justify-end flex-col lg:flex-row items-end ${raleway.className}`}>
+    <div className={`flex justify-end flex-col items-end ${raleway.className}`}>
       <div className='flex w-fit border-b-8 border-blue-500 items-end'>
         <h1 className='text-3xl s:text-4xl md:text-5xl text-neutral-600 font-thin -mb-2 sm-pl-20 md:pl-64 lg:pl-72 xl:pl-80 2xl:pl-96'>KEIHOKU STUDIO</h1>
-        <p className='px-4 md:px-6 md:px-10 lg:px-12 xl:px-14  2xl:px-16 text-neutral-400 inline-block -mb-1 text-lg'>à La Dérive</p>
       </div>
+        <p className='px-4 md:px-6 md:px-10 lg:px-12 xl:px-14  2xl:px-16 text-neutral-400 inline-block -mb-1 text-lg'>Pratiques de CORPS/DANSE/SHIATSU/ à La Dérive</p>
     </div>
   )
 }
