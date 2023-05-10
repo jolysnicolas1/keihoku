0aa0a88cc6b285467d2d902cac3ff19c52ba85bb
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-10 13:02:56 +0200

    responsive next events

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const raleway = Raleway({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="min-h-screen pt-12 flex flex-col space-y-24">
+    <main className="min-h-screen pt-12 flex flex-col space-y-12">
       {/* <div className="flex flex-col justify-center font-medium">
         <Studio></Studio>
         <div className='agenda text-right my-5'>
@@ -40,23 +40,23 @@ export default function Home() {
 
       <Footer></Footer> */}
 
-      <div className="flex flex-col p-2 space-y-5 font-light">
-        <div className='flex flex-col'>
-          <p className=''> <strong className='text-red-600'>FORMATION.SHIATSU</strong><strong className='text-cyan-200'>/ALAIN.TAUSH</strong>  </p>
+      <div className="flex flex-col items-end p-1 sm:p-4 space-y-5 font-light sm:text-xl lg:px-12 xl:px-24">
+        <div className='flex flex-col items-end'>
+          <p className=''> <strong className='text-red-600'>FORMATION.SHIATSU</strong><strong className='text-cyan-200'>/ALAIN.TAUSH</strong></p>
           <p className=''><strong className='text-cyan-200'>/MANABU.WATANABE</strong><strong className='text-neutral-400'>/09/2023-05/2024</strong></p>
         </div>
-        <div className='flex flex-col'>
-          <p className=''> <strong className='text-cyan-200'>INITIATION.SHIATSU</strong><strong className='text-blue-500'>/ALAIN.TAUSH</strong>  </p>
+        <div className='flex flex-col items-end'>
+          <p className=''> <strong className='text-cyan-200'>INITIATION.SHIATSU</strong><strong className='text-blue-500'>/ALAIN.TAUSH</strong></p>
           <p className=''><strong className='text-blue-500'>/MANABU.WATANABE</strong><strong className='text-neutral-400'>/01-07/023/02-07/023</strong></p>
         </div>
-        <div className='flex flex-col'>
-          <p className=''> <strong className='text-red-600'>ÇA.SE.FAIT #3</strong><strong className='text-cyan-200'>/ATELIER.DE.DANSE</strong>  </p>
+        <div className='flex flex-col items-end'>
+          <p className=''> <strong className='text-red-600'>ÇA.SE.FAIT #3</strong><strong className='text-cyan-200'>/ATELIER.DE.DANSE</strong></p>
           <p className=''><strong className='text-blue-500'>/ÉMILIA.GIUDICELLI</strong><strong className='text-neutral-400'>/11-06/2023</strong></p>
         </div>
+      </div>
       <Link href="/butoh">
         <NextEventSm/>
       </Link>
-      </div>
 
     </main>
   )
