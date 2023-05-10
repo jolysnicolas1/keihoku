772f3f8de40102a877b8585fe7dd6a488f243323
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-10 17:02:54 +0200

    Remove cursor

--- app/Cursor.tsx
+++ app/Cursor.tsx
@@ -4,28 +4,29 @@ import { useEffect } from "react";
 
 
 export default function CustomCursor() {
-    const cursorRef = useRef(null)
-    useEffect(() => {
-    if (cursorRef.current == null || cursorRef == null)
-        return;
-    document.addEventListener('mousemove', e => {
-        if (cursorRef.current == null)
-            return;
-        cursorRef.current.setAttribute("style", "top: " + (e.pageY) + "px; left: " + (e.pageX) + "px;")
-    })
-    document.addEventListener('click', () => {
-    if (cursorRef.current == null)
-        return;
-    cursorRef.current.classList.add("expand");
-    setTimeout(() => {
-        if (cursorRef.current == null)
-            return;
-        cursorRef.current.classList.remove("expand");
-    }, 500)
-    })
-    }, [])
+    // const cursorRef = useRef(null)
+    // useEffect(() => {
+    // if (cursorRef.current == null || cursorRef == null)
+    //     return;
+    // document.addEventListener('mousemove', e => {
+    //     if (cursorRef.current == null)
+    //         return;
+    //     cursorRef.current.setAttribute("style", "top: " + (e.pageY) + "px; left: " + (e.pageX) + "px;")
+    // })
+    // document.addEventListener('click', () => {
+    // if (cursorRef.current == null)
+    //     return;
+    // cursorRef.current.classList.add("expand");
+    // setTimeout(() => {
+    //     if (cursorRef.current == null)
+    //         return;
+    //     cursorRef.current.classList.remove("expand");
+    // }, 500)
+    // })
+    // }, [])
     return (
-        <div className="cursor" ref={cursorRef}>
+        // <div className="cursor" ref={cursorRef}>
+        <div className="cursor">
 
         </div>
     )
