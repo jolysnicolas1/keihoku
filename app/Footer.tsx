60f6ba0141cb56acb6450cf5c3dd07ad3f593fb9
nicolas jolys <jolys.nicolas@gmail.com>
2023-05-24 10:36:33 +0200

    Fix email address

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -7,7 +7,7 @@ const raleway = Raleway({ subsets: ['latin'] });
 export default function Footer() {
   return (
     <footer className='flex justify-center space-x-5 p-5 text-neutral-500'>
-        <p>keyhoku.studio@gmail.com</p>
+        <p>keihoku.studio@gmail.com</p>
         <p>07.56.98.00.85</p>
     </footer>
   )
