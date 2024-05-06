193ad4c1c9d9222ef5c8db9b67afacee5b5a7e77
jolysnicolas1 <jolys.nicolas1@gmail.com>
2024-05-06 22:09:50 +0200

    update

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const navigation = [
-]
-
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
@@ -50,17 +47,6 @@ export default function Example() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/25">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
             </div>
           </div>
         </Dialog.Panel>
